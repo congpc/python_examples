a3c5deea20619173ec78874f1d79b4627a0b4ae7
Pham Chi Cong <congpc@trans-tech.vn>
2025-04-29 14:37:08 +0700

    Add sqlite sample

--- one_app/app.py
+++ one_app/app.py
@@ -11,9 +11,11 @@ DATA_URL = "https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data
 @st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
+
     # lowercase = lambda x: str(x).lower()
     def lowercase(x):
         return str(x).lower()
+
     data.rename(lowercase, axis="columns", inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
